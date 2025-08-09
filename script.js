ba5e9c0c74eccde5ffa970cfd0cf554efc9bca4d
Luc1100 <52473174+Luc1100@users.noreply.github.com>
2025-08-09 17:10:23 -0400

    Removed old text based play functionality

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function getComputerChoice() {
-    let choice = Math.random() * 3
+    let choice = Math.random() * 3;
     if (choice < 1) {
-        return("rock")
+        return("rock");
     } else if (choice < 2) {
-        return("paper")
+        return("paper");
     } else {
-        return("scissors")
+        return("scissors");
     }
 
 }
 
 function getHumanChoice() {
-    return(prompt("Rock, Paper, or Scissors?"))
+    return(prompt("Rock, Paper, or Scissors?"));
 }
 
 function playRound(humanSelection, computerSelection) {
@@ -20,29 +20,29 @@ function playRound(humanSelection, computerSelection) {
         humanSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
         resultsDiv.textContent = `${humanSelection} beats ${computerSelection}!`;
         humanScore++;
-        updateScore()
+        updateScore();
     } else if (humanSelection.toLowerCase() == "rock" && computerSelection == "paper" ||
             humanSelection.toLowerCase() == "paper" && computerSelection == "scissors" ||
             humanSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
         resultsDiv.textContent = `${humanSelection} loses to ${computerSelection}!`
         computerScore++;
-        updateScore()
+        updateScore();
     } else if (humanSelection.toLowerCase() == computerSelection) {
-        resultsDiv.textContent = `${humanSelection} ties ${computerSelection}!`
+        resultsDiv.textContent = `${humanSelection} ties ${computerSelection}!`;
     }
 }
 
 function updateScore() {
     if (humanScore == 5) { 
-        scoreDiv.textContent = `You win! You ${humanScore} - ${computerScore} Computer`
-        computerScore = 0
-        humanScore = 0
+        scoreDiv.textContent = `You win! You ${humanScore} - ${computerScore} Computer`;
+        computerScore = 0;
+        humanScore = 0;
     } else if (computerScore == 5) {
-        scoreDiv.textContent = `You lose! You ${humanScore} - ${computerScore} Computer`
-        computerScore = 0
-        humanScore = 0
+        scoreDiv.textContent = `You lose! You ${humanScore} - ${computerScore} Computer`;
+        computerScore = 0;
+        humanScore = 0;
     } else {
-        scoreDiv.textContent = `You ${humanScore} - ${computerScore} Computer`
+        scoreDiv.textContent = `You ${humanScore} - ${computerScore} Computer`;
     } 
 }
 
@@ -51,18 +51,18 @@ const paperBtn = document.querySelector("#paper-btn");
 const scissorsBtn = document.querySelector("#scissors-btn");
 
 const resultsDiv = document.querySelector("#results-div");
-const scoreDiv = document.querySelector("#scores-div")
+const scoreDiv = document.querySelector("#scores-div");
 let humanScore = 0;
 let computerScore = 0;
 
 rockBtn.addEventListener("click", () => {
-    playRound("rock", getComputerChoice())
+    playRound("rock", getComputerChoice());
 });
 
 paperBtn.addEventListener("click", () => {
-    playRound("paper", getComputerChoice())
+    playRound("paper", getComputerChoice());
 });
 
 scissorsBtn.addEventListener("click", () => {
-    playRound("scissors", getComputerChoice())
+    playRound("scissors", getComputerChoice());
 });
